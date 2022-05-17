c03052ccaaf563b6cafd6112180c27e0db0e1fe8
eventide040 <eventide666@protonmail.com>
2022-05-17 18:08:01 +0200

    cośtam

--- core/src/main/resources/static/js/script.js
+++ core/src/main/resources/static/js/script.js
@@ -1,32 +1,302 @@
+const body = document.getElementsByTagName("body")[0]
+
+const ip = 'http://localhost'
+const port = '8080'
+const apiVersion = 'v1'
+
+let enemyAnimation
+let playerAnimation
+let enemyLeft
+let playerLeft
+let height
+let width
+let yourTurn
+let enemyTurn
+
+let playerTurn
+let gameId
+let playerId
+let enemyId
+let ships
+
+connect();
+
 function connect() {
     const socket = new SockJS('/websocket')
     let stompClient = Stomp.over(socket)
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame)
         stompClient.subscribe('/user/queue/notification', async function (message) {
-            console.log(message)
             let event = JSON.parse(message.body)
             let eventType = event['eventType']
             let object = event['content']
-            console.log(eventType)
-            console.log(object)
+            switch (eventType) {
+                case "EXCEPTION": {
+                    break
+                }
+                case "SERVER_ERROR": {
+                    break
+                }
+                case "GAME_CREATED": {
+                    gameId = object['gameId']
+                    createGameIdElement()
+                    requestBoard()
+                    startGameButton()
+                    break
+                }
+                case "GAME_JOINED": {
+                    // gameId = object['gameId']
+                    playerId = object['playerId']
+                    enemyId = object['enemyId']
+                    createGameIdElement()
+                    requestPlacement()
+
+
+                    break
+                }
+                case "BOARD_CREATED": {
+                    width = object['width']
+                    height = object['height']
+                    break
+                }
+                case "FLEET_CREATED": {
+                    ships = object['ships']
+                    initializeBoard('player', ships)
+                    initializeBoard('enemy', ships)
+                    break
+                }
+                case "GAME_STARTED": {
+                    playerTurn = object['playerTurn']
+                    setUpBoardsBasedOnPlayerTurn()
+                    break
+                }
+                case "GAME_END": {
+                    break
+                }
+                case "HIT": {
+                    break
+                }
+            }
         })
     });
 }
 
-connect();
+function startGame() {
+    const requestURL = "http://localhost:8080/api/v1/games/" + gameId + "/start"
+    const request = new Request(requestURL, {
+        method: 'POST',
+        mode: 'cors'
+    })
+    const response = fetch(request)
+    document.getElementById("boardContainer").removeChild(document.getElementById('startGameButton'))
+}
 
-function send() {
-    fetch("http://localhost:8080/api/v1/games", {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    }).then(r => console.log(r));
+function setUpBoardsBasedOnPlayerTurn() {
+    if (playerTurn === playerId) {
+        enemyAnimation = 'fadein'
+        playerAnimation = 'fadeout'
+        enemyLeft = '30vw'
+        playerLeft = '-50vw'
+        yourTurn = "YOUR TURN"
+        enemyTurn = "OPPONENT's TURN"
+    } else {
+        enemyAnimation = 'fadeout'
+        playerAnimation = 'fadein'
+        enemyLeft = '-50vw'
+        playerLeft = '30vw'
+        yourTurn = "OPPONENT's TURN"
+        enemyTurn = "YOUR TURN"
+    }
+    document.getElementById('enemy').style.left = enemyLeft
+    document.getElementById('enemy').classList.add(enemyAnimation)
+    document.getElementById('player').style.left = playerLeft
+    document.getElementById('player').classList.add(playerAnimation)
+}
+
+//TODO: ERROR HANDLING
+function requestPlacement() {
+    const requestURL = "http://localhost:8080/api/v1/games/" + gameId + "/placements"
+    const request = new Request(requestURL, {
+        method: 'POST',
+        mode: 'cors'
+    })
+    const response = fetch(request)
+}
+
+function startGameButton() {
+    let button = document.createElement('button')
+    button.setAttribute('onclick', "startGame()")
+    button.setAttribute('id', 'startGameButton')
+    button.classList.add('menuButton')
+    button.classList.add('gameCreate')
+    button.classList.add('text')
+    button.innerText = 'Start Game'
+    document.getElementById('boardContainer').appendChild(button)
+}
+
+//TODO: ERROR HANDLING | USER CAN SET BOARD SIZE
+function requestBoard() {
+    const requestURL = "http://localhost:8080/api/v1/games/" + gameId + "/boards"
+    const board = {
+        "width": 10,
+        "height": 10
+    }
+    const header = new Headers()
+    header.append('Content-Type', 'application/json')
+    const request = new Request(requestURL, {
+        method: 'POST',
+        body: JSON.stringify(board),
+        headers: header,
+        mode: 'cors'
+    })
+    const response = fetch(request)
+}
+
+function initializeBoard(type, fleet) {
+    let container = document.createElement("div")
+    container.classList.add('container')
+    container.classList.add(type)
+    container.setAttribute('id', type)
+
+    let horizontalCoordinates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T']
+
+    for (let i = 1; i <= width * height; i++) {
+        let cell = document.createElement("div")
+        cell.classList.add('cell')
+        cell.setAttribute('id', i.toString() + ':' + type)
+
+        if (type === 'enemy') {
+            cell.setAttribute('onclick', 'shoot(this)')
+            cell.style.cursor = 'crosshair'
+
+            let coordinate = horizontalCoordinates[i % 10] + +(((i / width * 10 - i % 10) / 10) + 1)
+            let displayCellCoordinate = document.createElement('div')
+            displayCellCoordinate.classList.add('cellCoordinate')
+            displayCellCoordinate.textContent = coordinate
+            cell.appendChild(displayCellCoordinate)
+        }
+        container.appendChild(cell)
+    }
+    body.appendChild(container)
+
+    createShips(fleet, type).then(() => {
+        if (type === 'enemy') {
+            document.getElementById('enemy').style.left = enemyLeft
+            document.getElementById('enemy').classList.add(enemyAnimation)
+        } else {
+            document.getElementById('player').style.left = playerLeft
+            document.getElementById('player').classList.add(playerAnimation)
+        }
+    })
+}
+
+async function createShips(fleet, type) {
+    if (type === 'player') {
+        fleet.forEach(ship => ship["masts"].forEach(cellID => {
+            document.getElementById(cellID + ":" + type).classList.add("ship")
+        }))
+    }
+}
+
+//TODO: ERROR HANDLING
+
+async function createGame() {
+    await resetBoardContainer()
+    const requestURL = "http://localhost:8080/api/v1/games"
+    const request = new Request(requestURL, {
+        method: 'POST',
+        mode: 'cors',
+    })
+    const response = fetch(request)
+    console.log(response);
+}
+
+async function joinGame(gameId) {
+    await resetBoardContainer()
+    const requestURL = "http://localhost:8080/api/v1/games/" + gameId + "/join"
+    const request = new Request(requestURL, {
+        method: 'POST',
+        mode: 'cors',
+    })
+    const response = fetch(request)
+    console.log(response);
+}
+
+async function resetBoardContainer() {
+    const boardContainer = document.getElementById("boardContainer")
+    boardContainer.removeChild(document.getElementById("gameCreate"))
+    boardContainer.removeChild(document.getElementById("gameJoin"))
+    boardContainer.style.backgroundColor = "#3b4252"
+    boardContainer.style.borderColor = "#3b4252"
+}
+
+function swapBoards() {
+
+    if (document.getElementById('turn').innerText === enemyTurn) {
+        document.getElementById('turn').innerText = yourTurn
+    } else if (document.getElementById('turn').innerText === yourTurn) {
+        document.getElementById('turn').innerText = enemyTurn
+    }
+
+    document.getElementById('enemy').style.left = playerLeft
+    document.getElementById('player').style.left = enemyLeft
+
+    let temp1 = playerLeft
+    playerLeft = enemyLeft
+    enemyLeft = temp1
+
+    boardAnimation()
+}
+
+
+function getGameIDFromPlayer() {
+    const gameIdFromPlayer = window.prompt("Enter game id: ")
+    if (gameIdFromPlayer.length > 0) {
+        joinGame(gameIdFromPlayer).then(() => {
+        })
+    }
+    gameId = gameIdFromPlayer
+}
+
+function boardAnimation() {
+    document.getElementById('player').classList.add(enemyAnimation)
+    document.getElementById('player').classList.remove(playerAnimation)
+    document.getElementById('enemy').classList.add(playerAnimation)
+    document.getElementById('enemy').classList.remove(enemyAnimation)
+
+    let temp = playerAnimation
+    playerAnimation = enemyAnimation
+    enemyAnimation = temp
+}
+
+function activateCells() {
+    let cells = document.getElementsByClassName('cell')
+    for (let cellElement of cells) {
+        if (cellElement.id.toString().includes('enemy')) {
+            cellElement.classList.remove('deactivate')
+        }
+    }
+}
+
+function deactivateCells() {
+    let cells = document.getElementsByClassName('cell')
+    for (let cellElement of cells) {
+        if (cellElement.id.toString().includes('enemy')) {
+            cellElement.classList.add('deactivate')
+        }
+    }
+}
+
+const delay = millis => new Promise((resolve, reject) => {
+    setTimeout(_ => resolve(), millis)
+});
+
+function createGameIdElement() {
+    if (document.getElementById('gameId') !== null) return
+    let displayId = document.createElement('p')
+    displayId.setAttribute('id', 'gameId')
+    displayId.classList.add('gameId')
+    displayId.textContent = 'game-id: ' + gameId
+    body.appendChild(displayId)
 }

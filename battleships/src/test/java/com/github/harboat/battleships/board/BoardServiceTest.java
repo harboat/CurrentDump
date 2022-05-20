9632a7ca7fbc8419e1f07305ef3223b942c4f91a
Anna Ovcharenko <annaowczarenko@gmail.com>
2022-05-20 09:35:37 +0200

    Repair tests

--- battleships/src/test/java/com/github/harboat/battleships/board/BoardServiceTest.java
+++ battleships/src/test/java/com/github/harboat/battleships/board/BoardServiceTest.java
@@ -1,6 +1,5 @@
 package com.github.harboat.battleships.board;
 
-import com.github.harboat.battleships.CoreQueueProducer;
 import com.github.harboat.clients.game.Size;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -18,121 +17,113 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.mockito.BDDMockito.*;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertSame;
+import static org.testng.Assert.*;
 
 @Listeners({MockitoTestNGListener.class})
 public class BoardServiceTest {
 
-//    @Mock
-//    private BoardRepository repository;
-//    @Mock
-//    private CoreQueueProducer producer;
-//    private BoardService service;
-//    private final String gameId = "test";
-//    private final String playerId = "testPlayer";
-//    private final Size size = new Size(10, 10);
-//    @Captor
-//    ArgumentCaptor<BoardCreationResponse> captor;
-//    @Captor
-//    ArgumentCaptor<Board> boardCaptor;
-//
-//    @BeforeMethod
-//    public void setUp() {
-//        service = new BoardService(repository, producer);
-//    }
-//
-//    @Test
-//    public void shouldCreateBoardWithProperGameId() {
-//        //given
-//        given(repository.save(any())).willReturn(null);
-//        //when
-//        service.createBoard(new BoardCreation(gameId, playerId, size));
-//        verify(producer).sendResponse(captor.capture());
-//        BoardCreationResponse actual = captor.getValue();
-//        //then
-//        assertEquals(actual.gameId(), gameId);
-//    }
-//
-//    @Test
-//    public void shouldCreateBoardWithProperPlayerId() {
-//        //given
-//        given(repository.save(any())).willReturn(null);
-//        //when
-//        service.createBoard(new BoardCreation(gameId, playerId, size));
-//        verify(producer).sendResponse(captor.capture());
-//        BoardCreationResponse actual = captor.getValue();
-//        //then
-//        assertEquals(actual.playerId(), playerId);
-//    }
-//
-//    @Test
-//    public void shouldCreateBoardWithProperSize() {
-//        //given
-//        given(repository.save(any())).willReturn(null);
-//        //when
-//        service.createBoard(new BoardCreation(gameId, playerId, size));
-//        verify(producer).sendResponse(captor.capture());
-//        BoardCreationResponse actual = captor.getValue();
-//        //then
-//        assertEquals(actual.size(), size);
-//    }
-//
-//    @DataProvider
-//    public static Object[][] listOfCellsFor10x10Board() {
-//        return new Object[][]{
-//                {List.of(1, 2, 15)}
-//                , {List.of(1)}
-//                , {List.of(1, 100)}
-//        };
-//    }
-//
-//    @Test(dataProvider = "listOfCellsFor10x10Board")
-//    public void shouldMarkOccupied(Collection<Integer> cells) {
-//        //given
-//        Board board = Board.builder()
-//                .gameId(gameId)
-//                .playerId(playerId)
-//                .size(size)
-//                .cells(IntStream.rangeClosed(1, 10 * 10).boxed()
-//                        .collect(Collectors.toMap((i) -> i, (i) -> Cell.WATER)))
-//                .build();
-//        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.ofNullable(board));
-//        //when
-//        service.markOccupied(gameId, playerId, cells);
-//        verify(repository).save(boardCaptor.capture());
-//        var actual = boardCaptor.getValue();
-//        //then
-//        assertEquals(actual.getCells().keySet().stream()
-//                .filter(i -> actual.getCells().get(i) == Cell.OCCUPIED)
-//                .collect(Collectors.toList()), cells);
-//    }
-//
-//    @DataProvider
-//    public static Object[][] cellIdsFor10x10Board() {
-//        return new Object[][]{
-//                {1}
-//                , {100}
-//                , {10}
-//        };
-//    }
-//
-//    @Test(dataProvider = "cellIdsFor10x10Board")
-//    public void shouldMarkHit(int cellId) {
-//        //given
-//        Board board = Board.builder()
-//                .gameId(gameId)
-//                .playerId(playerId)
-//                .size(size)
-//                .cells(IntStream.rangeClosed(1, 10 * 10).boxed()
-//                        .collect(Collectors.toMap((i) -> i, (i) -> Cell.WATER)))
-//                .build();
-//        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.ofNullable(board));
-//        //when
-//        service.markHit(gameId, playerId, cellId);
-//        verify(repository).save(boardCaptor.capture());
-//        var actual = boardCaptor.getValue();
-//        //then
-//        assertSame(actual.getCells().get(cellId), Cell.HIT);
-//    }
+    @Mock
+    private BoardRepository repository;
+    private BoardService service;
+    private final String gameId = "test";
+    private final String playerId = "testPlayer";
+    private final Size size = new Size(10, 10);
+    @Captor
+    ArgumentCaptor<Board> boardCaptor;
+
+    @BeforeMethod
+    public void setUp() {
+        service = new BoardService(repository);
+    }
+
+    @Test
+    public void shouldCreateBoardWithProperGameId() {
+        //given
+        //when
+        service.createBoard(gameId,playerId,size);
+        verify(repository).save(boardCaptor.capture());
+        var actual = boardCaptor.getValue();
+        //then
+        assertEquals(actual.getGameId(), gameId);
+    }
+
+    @Test
+    public void shouldCreateBoardWithProperPlayerId() {
+        //given
+        //when
+        service.createBoard(gameId,playerId,size);
+        verify(repository).save(boardCaptor.capture());
+        var actual = boardCaptor.getValue();
+        //then
+        assertEquals(actual.getPlayerId(), playerId);
+    }
+
+    @Test
+    public void shouldCreateBoardWithProperSize() {
+        //given
+        //when
+        service.createBoard(gameId,playerId,size);
+        verify(repository).save(boardCaptor.capture());
+        var actual = boardCaptor.getValue();
+        //then
+        assertEquals(actual.getSize(), size);
+    }
+
+    @DataProvider
+    public static Object[][] listOfCellsFor10x10Board() {
+        return new Object[][]{
+                {List.of(1, 2, 15)}
+                , {List.of(1)}
+                , {List.of(1, 100)}
+        };
+    }
+
+    @Test(dataProvider = "listOfCellsFor10x10Board")
+    public void shouldMarkOccupied(Collection<Integer> cells) {
+        //given
+        Board board = Board.builder()
+                .gameId(gameId)
+                .playerId(playerId)
+                .size(size)
+                .cells(IntStream.rangeClosed(1, 10 * 10).boxed()
+                        .collect(Collectors.toMap((i) -> i, (i) -> Cell.WATER)))
+                .build();
+        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.ofNullable(board));
+        //when
+        service.markOccupied(gameId, playerId, cells);
+        verify(repository).save(boardCaptor.capture());
+        var actual = boardCaptor.getValue();
+        //then
+        assertEquals(actual.getCells().keySet().stream()
+                .filter(i -> actual.getCells().get(i) == Cell.OCCUPIED)
+                .collect(Collectors.toList()), cells);
+    }
+
+    @DataProvider
+    public static Object[][] cellIdsFor10x10Board() {
+        return new Object[][]{
+                {1}
+                , {100}
+                , {10}
+        };
+    }
+
+    @Test(dataProvider = "cellIdsFor10x10Board")
+    public void shouldMarkHit(int cellId) {
+        //given
+        Board board = Board.builder()
+                .gameId(gameId)
+                .playerId(playerId)
+                .size(size)
+                .cells(IntStream.rangeClosed(1, 10 * 10).boxed()
+                        .collect(Collectors.toMap((i) -> i, (i) -> Cell.WATER)))
+                .build();
+        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.ofNullable(board));
+        //when
+        service.markHit(gameId, playerId, cellId);
+        verify(repository).save(boardCaptor.capture());
+        var actual = boardCaptor.getValue();
+        //then
+        assertSame(actual.getCells().get(cellId), Cell.HIT);
+    }
 }
--- battleships/src/test/java/com/github/harboat/battleships/fleet/FleetServiceTest.java
+++ battleships/src/test/java/com/github/harboat/battleships/fleet/FleetServiceTest.java
@@ -4,9 +4,8 @@ import com.github.harboat.battleships.CoreQueueProducer;
 import com.github.harboat.battleships.NotificationProducer;
 import com.github.harboat.battleships.board.BoardService;
 import com.github.harboat.battleships.game.GameUtility;
-import com.github.harboat.clients.game.ShipDto;
 import com.github.harboat.clients.game.*;
-import com.github.harboat.clients.notification.EventType;
+import com.github.harboat.clients.game.ShipDto;
 import com.github.harboat.clients.notification.NotificationRequest;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -18,136 +17,119 @@ import org.testng.annotations.Test;
 
 import java.util.*;
 
-import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.verify;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.mockito.BDDMockito.*;
+import static org.testng.Assert.*;
 
 @Listeners({MockitoTestNGListener.class})
 public class FleetServiceTest {
 
-//    @Mock
-//    private FleetRepository repository;
-//    @Mock
-//    private GameUtility gameUtility;
-//    @Mock
-//    private NotificationProducer producer;
-//    @Mock
-//    private CoreQueueProducer coreQueueProducer;
-//    @Mock
-//    private BoardService boardService;
-//    private FleetService service;
-//    @Captor
-//    private ArgumentCaptor<NotificationRequest<FleetDto>> notificationRequestCaptor;
-//    @Captor
-//    private ArgumentCaptor<PlacementResponse> placementResponseCaptor;
-//    @Captor
-//    private ArgumentCaptor<ShotResponse> shotResponseCaptor;
-//    @Captor
-//    private ArgumentCaptor<PlayerWon> playerWonCaptor;
-//    private ShipDto ship1;
-//    private ShipDto ship2;
-//
-//
-//    @BeforeMethod
-//    public void setUp() {
-//        service = new FleetService(repository, gameUtility, producer, coreQueueProducer, boardService);
-//        ship1 = new ShipDto(ShipType.DESTROYER, new Masts(List.of(1)), new OccupiedCells(List.of(2, 11, 12)));
-//        ship2 = new ShipDto(ShipType.DESTROYER, new Masts(List.of(3, 4)), new OccupiedCells(List.of(2, 12, 13, 14, 15, 5)));
-//    }
-//
-//    @Test
-//    public void shouldConvertMasts() {
-//        //given
-//        Masts masts = new Masts(List.of(1, 2, 3));
-//        //when
-//        MastsState actual = service.convertMasts(masts);
+    @Mock
+    private FleetRepository repository;
+    @Mock
+    private GameUtility gameUtility;
+    @Mock
+    private NotificationProducer producer;
+    @Mock
+    private CoreQueueProducer coreQueueProducer;
+    @Mock
+    private BoardService boardService;
+    private FleetService service;
+    @Captor
+    private ArgumentCaptor<NotificationRequest<FleetDto>> notificationRequestCaptor;
+    @Captor
+    private ArgumentCaptor<Fleet> fleetCaptor;
+    private com.github.harboat.clients.game.ShipDto ship1;
+    private com.github.harboat.clients.game.ShipDto ship2;
+
+
+    @BeforeMethod
+    public void setUp() {
+        service = new FleetService(repository, gameUtility, producer, coreQueueProducer, boardService);
+        ship1 = new com.github.harboat.clients.game.ShipDto(ShipType.DESTROYER,
+                new Masts(List.of(1)),
+                new OccupiedCells(List.of(2, 11, 12)));
+        ship2 = new com.github.harboat.clients.game.ShipDto(ShipType.DESTROYER,
+                new Masts(List.of(3, 4)),
+                new OccupiedCells(List.of(2, 12, 13, 14, 15, 5)));
+    }
+
+    @Test
+    public void shouldConvertMasts() {
+        //given
+        //when
+        MastsState actual = service.convertMasts(List.of(1, 2, 3));
+        //then
+        assertEquals(actual.getMasts(), new MastsState(new HashMap<Integer, MastState>() {{
+            put(1, MastState.ALIVE);
+            put(2, MastState.ALIVE);
+            put(3, MastState.ALIVE);
+        }}).getMasts());
+    }
+
+    @Test
+    public void shouldCreateFleetWithProperPlayerId() {
+        //given
+        Collection<ShipDto> collection = List.of(ship1, ship2);
+        //when
+        service.create("test", "testPlayer", collection);
+        verify(repository).save(fleetCaptor.capture());
+        var actual = fleetCaptor.getValue();
+        //then
+        assertEquals(actual.getPlayerId(), "testPlayer");
+    }
+
+    @Test
+    public void shouldCreateFleetWithProperFleet() {
+        //given
+        Collection<ShipDto> collection = List.of(ship1, ship2);
+        //when
+        service.create("test", "testPlayer", collection);
+        verify(repository).save(fleetCaptor.capture());
+        var actual = fleetCaptor.getValue();
+        //then
+        assertEquals(actual.getShips().size(), collection.size());
+    }
+
+    @Test
+    public void shouldShootAndMiss() {
+        //given
+        ArgumentCaptor<ShotResponse> shotResponseCaptor = ArgumentCaptor.forClass(ShotResponse.class);
+        Ship ship = new Ship(ShipType.SUBMARINE,
+                new MastsState(new HashMap<Integer, MastState>() {{
+                    put(3, MastState.ALIVE);
+                    put(4, MastState.ALIVE);
+                }}),
+                new OccupiedCells(Arrays.asList(2, 12, 13, 14, 15, 5)));
+        Fleet fleet = new Fleet("testFleet", "test", "testPlayer", List.of(ship));
+        ShotRequest shotRequest = new ShotRequest("test", "testPlayer", 10);
+        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.of(fleet));
+        //when
+        service.shoot(shotRequest);
+        verify(coreQueueProducer).sendResponse(shotResponseCaptor.capture());
+        var actual = shotResponseCaptor.getValue();
+        //then
+        assertTrue(actual.cells().contains(new Cell(10, false)));
+    }
+
+    @Test
+    public void shouldShootAndSunk() {
+        //given
+        ArgumentCaptor<ShotResponse> shotResponseCaptor = ArgumentCaptor.forClass(ShotResponse.class);
+        Ship ship = new Ship(ShipType.DESTROYER,
+                new MastsState(new HashMap<Integer, MastState>() {{
+                    put(1, MastState.ALIVE);
+                }}),
+                new OccupiedCells(Arrays.asList(2, 11, 12)));
+        Fleet fleet = new Fleet("testFleet", "test", "testPlayer", List.of(ship));
+        ShotRequest shotRequest = new ShotRequest("test", "testPlayer", 1);
+        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.of(fleet));
+        doNothing().when(coreQueueProducer).sendResponse(shotResponseCaptor.capture());
+        //when
+        service.shoot(shotRequest);
+        verify(coreQueueProducer, times(2)).sendResponse(any());
+//        var actual = playerWonCaptor.getValue();
 //        //then
-//        assertEquals(actual.getMasts(), new MastsState(new HashMap<Integer, MastState>() {{
-//            put(1, MastState.ALIVE);
-//            put(2, MastState.ALIVE);
-//            put(3, MastState.ALIVE);
-//        }}).getMasts());
-//    }
-//
-//    @Test
-//    public void shouldCreateFleetWithProperEventType() {
-//        //given
-//        Collection<ShipDto> collection = List.of(ship1, ship2);
-//        //when
-//        service.create(new GamePlacement("test", "testPlayer", collection));
-//        verify(producer).sendNotification(notificationRequestCaptor.capture());
-//        verify(coreQueueProducer).sendResponse(placementResponseCaptor.capture());
-//        NotificationRequest<FleetDto> actual = notificationRequestCaptor.getValue();
-//        //then
-//        assertEquals(actual.type(), EventType.FLEET_CREATED);
-//    }
-//
-//    @Test
-//    public void shouldCreateFleetWithProperPlayerId() {
-//        //given
-//        Collection<ShipDto> collection = List.of(ship1, ship2);
-//        //when
-//        service.create(new GamePlacement("test", "testPlayer", collection));
-//        verify(producer).sendNotification(notificationRequestCaptor.capture());
-//        verify(coreQueueProducer).sendResponse(placementResponseCaptor.capture());
-//        NotificationRequest<FleetDto> actual = notificationRequestCaptor.getValue();
-//        //then
-//        assertEquals(actual.userId(), "testPlayer");
-//    }
-//
-////    @Test
-////    public void shouldCreateFleetWithProperFleet() {
-////        //given
-////        Collection<ShipDto> collection = List.of(ship1, ship2);
-////        //when
-////        service.create(new GamePlacement("test", "testPlayer", collection));
-////        verify(producer).sendNotification(notificationRequestCaptor.capture());
-////        verify(coreQueueProducer).sendResponse(placementResponseCaptor.capture());
-////        NotificationRequest<FleetDto> actual = notificationRequestCaptor.getValue();
-////        //then
-////        System.out.println(actual.body().getShips());
-////        assertEquals(actual.body().getShips(),collection );
-////    }
-//
-//    @Test
-//    public void shouldShootAndMiss() {
-//        //given
-//        Ship ship = new Ship(ShipType.SUBMARINE,
-//                new MastsState(new HashMap<Integer, MastState>() {{
-//                    put(3, MastState.ALIVE);
-//                    put(4, MastState.ALIVE);
-//                }}),
-//                new OccupiedCells(Arrays.asList(2, 12, 13, 14, 15, 5)));
-//        Fleet fleet = new Fleet("testFleet", "test", "testPlayer", List.of(ship));
-//        ShotRequest shotRequest = new ShotRequest("test", "testPlayer", 10);
-//        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.of(fleet));
-//        //when
-//        service.shoot(shotRequest);
-//        verify(coreQueueProducer).sendResponse(shotResponseCaptor.capture());
-//        var actual = shotResponseCaptor.getValue();
-//        //then
-//        assertTrue(actual.cells().contains(new Cell(10, false)));
-//    }
-//
-//    @Test
-//    public void shouldShootAndSunk() {
-//        //given
-//        Ship ship = new Ship(ShipType.DESTROYER,
-//                new MastsState(new HashMap<Integer, MastState>() {{
-//                    put(1, MastState.ALIVE);
-//                }}),
-//                new OccupiedCells(Arrays.asList(2, 11, 12)));
-//        Fleet fleet = new Fleet("testFleet", "test", "testPlayer", List.of(ship));
-//        ShotRequest shotRequest = new ShotRequest("test", "testPlayer", 1);
-//        given(repository.findByGameIdAndPlayerId(any(), any())).willReturn(Optional.of(fleet));
-//        doNothing().when(coreQueueProducer).sendResponse(shotResponseCaptor.capture());
-//        //when
-//        service.shoot(shotRequest);
-//        verify(coreQueueProducer, times(2)).sendResponse(any());
-//
-////        var actual = playerWonCaptor.getValue();
-////        //then
-////        assertEquals(actual.playerId(), "testPlayer");
-//    }
+//        assertEquals(actual.playerId(), "testPlayer");
+    }
 }
